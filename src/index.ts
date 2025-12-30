ae16db4325bc47f1ed6f9cd55def06816883a627
ghifarij <afga.ghifari@gmail.com>
2025-12-30 18:02:17 +0700

    chore: Add .DS_Store to gitignore and apply minor formatting adjustments.

--- src/index.ts
+++ src/index.ts
@@ -74,10 +74,9 @@ app.get('/api', (req: Request, res: Response) => {
 
 // Start server
 const PORT = Number(process.env.PORT) || 8000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Listening on ${PORT}`);
 });
-
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err) => {
